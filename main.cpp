2d0fa7da5e96b21cbf8157231f80fbfe5b320057
lonleybeast <33063661+lonleybeast@users.noreply.github.com>
2021-06-08 12:28:03 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,43 @@
 #include <iostream>
 #include "Node.h"
-#include "Phonebook.h"
+#include "PhoneBook.h"
 using namespace std;
 
 
 int main()
 {
-    PhoneBook p;
+	PhoneBook p;
 
-    p.insert(new Node("Danish", "Lhr", "035420110"));
-    p.insert(new Node("Baba", "Lhr", "035420110"));
-    p.insert(new Node("Zalma", "Lhr", "035420110"));
-    p.insert(new Node("Zblma", "Lhr", "035420110"));
-    p.insert(new Node("Awais", "Swl", "0354235520110"));
-    p.insert(new Node("Aaaa", "Isl", "0334220110"));
-    p.insert(new Node("Abaa", "Isl", "0334220110"));
-    p.insert(new Node("Az", "Isl", "0334220110"));
+	p.insert(new Node("Danish", "LHR", "035420110"));
+	p.insert(new Node("Danish", "ISL", "34345453453453"));
+	p.insert(new Node("Baba", "LHR", "035420110"));
+	p.insert(new Node("Zalma", "SWL", "035420110"));
+	p.insert(new Node("Zblma", "LHR", "035420110"));
+	p.insert(new Node("Awais", "SWL", "0354235520110"));
+	p.insert(new Node("Aaaa", "ISL", "0334220110"));
+	p.insert(new Node("Abaa", "ISL", "0334220110"));
+	p.insert(new Node("Az", "ISL", "0334220110"));
+	system("cls");
+	string query = "Danish";
+	cout << "Querying '"<< query <<"': \n";
+	PhoneBook* result = p.searchByName(query);
+	if(result->size > 0) {
+		cout << result->size << " result found\n";
+		result->print();
+	} else {
+		cout << "No result found";
+	}
 
-    p.print();
+	query = "LHR";
+	cout << "Querying '"<< query <<"': \n";
+	result = p.searchByCity(query);
+	if(result->size > 0) {
+		cout << result->size << " results found\n";
+		result->print();
+	} else {
+		cout << "No result found";
+	}
+
+	// p.insert(new Node("Bhai", "ISL", "0334220110"));
+	//p.print();
 }
